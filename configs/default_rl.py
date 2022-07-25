cda7f23a1bff6cf0095ae124bf4a5908745c9297
cm8908 <jungminseob2@gmail.com>
2022-07-25 15:50:04 +0900

    config equal to xbresson 50

--- configs/default_rl.py
+++ configs/default_rl.py
@@ -6,6 +6,9 @@ args = DotDict()
 # Learning Method #
 args.rl = True
 args.loss_fn = 'reinforce'
+args.optim = 'adam'
+args.learning_rate = 0.001
+args.update_step = False
 args.update_intermediate = False
 args.update_total = True
 
@@ -13,33 +16,34 @@ args.update_total = True
 args.debug = True
 args.exp_dir = 'debug'
 args.seed = 1234
-args.eval_interval
+args.log_interval = 1
 
 # GPU #
 args.cuda = True
-args.gpu_id = '0,1'
+args.gpu_id = "7"
 args.multi_gpu = False
 
 # Data-Related #
 args.data_root = '../datasets'
 args.data_source = 'joshi'
 args.n_point = 50
-args.bsz = 1
+args.bsz = 512
 args.segm_len = 25
-
-# Optimizer #
-args.optim = 'adam'
+args.n_epoch = 10000
+args.rl_maxstep = 2500  # batch per epoch
+args.rl_eval_maxstep = 20
 
 # Model Hyperparameters #
-args.d_model = 1
-args.d_ff = 1
-args.n_head = 1
-args.n_layer = 1
+args.d_model = 128
+args.d_ff = 512
+args.n_head = 8
+args.n_enc_layer = 6
+args.n_dec_layer = 2
 
 # Minor Hyperparameters #
 args.deterministic = True
 args.pre_lnorm = True
-args.dropout_rate = 0.0
+args.dropout_rate = 0.1
 args.internal_drop = -1
-args.clip_value = -1
+args.clip_value = 10
 args.clamp_len = -1
